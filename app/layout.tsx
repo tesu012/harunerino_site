a1f1270309ea92ee5f3ed66a1c38ecb8a1f2f6f9
tesu012 <277407392+tesu012@users.noreply.github.com>
2026-04-18 21:36:45 +0900

    フォントの変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const mplusrounded1c = M_PLUS_Rounded_1c({
+  weight: "400",
+  variable: "--font-mplusrounded1c",
   subsets: ["latin"],
 });
 
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html
       lang="ja"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${mplusrounded1c.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
