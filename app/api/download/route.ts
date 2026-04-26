99a5126c31719201a5d7b038d888c6ee380d3b12
tesu012 <277407392+tesu012@users.noreply.github.com>
2026-04-26 19:50:39 +0900

    sound_source_version.tsでUTAU音源のバージョンを管理する

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -1,4 +1,6 @@
+import { UTAU_VERSION } from "@/lib/sound_source_version";
+
 export async function GET(request: Request) {
-  const url = new URL("/sound_source/harunerino_utau_v1.2.0.zip", request.url);
+  const url = new URL(`/sound_source/harunerino_utau_v${UTAU_VERSION}.zip`, request.url);
   return Response.redirect(url);
 }
---
+++ lib/sound_source_version.ts
@@ -0,0 +1,1 @@
+export const UTAU_VERSION = "1.2.0";
